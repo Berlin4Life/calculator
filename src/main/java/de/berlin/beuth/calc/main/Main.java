37ab661dcd9a7daca68a862d28d9846e2da0cd3e
Usman_Irshad <irshad@gmx.de>
2021-06-24 06:41:28 +0200

    call main api from main,

--- src/main/java/de/berlin/beuth/calc/main/Main.java
+++ src/main/java/de/berlin/beuth/calc/main/Main.java
@@ -1,11 +1,19 @@
 package de.berlin.beuth.calc.main;
 
 
+import de.berlin.beuth.calc.api.CalculatorApi;
+import de.berlin.beuth.calc.impl.CalculatorImpl;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        System.out.println("Hallo Beuth!!!!!");
-        System.out.println("Hallo Fuerth!!!!!");
+        CalculatorApi calculatorApi = new CalculatorImpl();
+
+        int countPictures = calculatorApi.countPictures("src/main/resources/inputtext.txt");
+
+        log.info("countPictures: "+countPictures);
     }
 }
